b7e0d25dc39d6ed9abc671693b3cfb5248c83d22
CasperKjaerhus <casperkjaerhusleth@gmail.com>
2020-03-18 15:38:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,6 @@ const {Messagetype, ServerLog} = require('./server/consoleLog.js');
 /*Options*/
 const option = new Options();
 
-/*Console Colors*/
-const Colors = {Alert: "\x1b[31m%s\x1b[0m", 
-                Notice: "\x1b[0m\x1b[33m%s\x1b[0m", 
-                Message: "\x1b[1m\x1b[30m%s\x1b[0m",
-                Success: "\x1b[32m%s\x1b[0m"};
-
 const server = http.createServer((req, res) => {
     res.writeHead(200);
     res.end();
@@ -27,8 +21,8 @@ async function startUp(){
     try{
         await option.load()
     }catch(e){
-        Console.log(Colors.Alert, "ERROR: LOADING OF OPTIONS DIDN'T HAPPEN");
+        ServerLog(Messagetype.alert,"ERROR: LOADING OF OPTIONS DIDN'T HAPPEN");
     }
     server.listen(option.http.port);
-    console.log(Colors.Success, `Server started! Listening on port ${option.http.port}`);
+    ServerLog(Messagetype.Success, `Server started! Listening on port ${option.http.port}`);
 }
